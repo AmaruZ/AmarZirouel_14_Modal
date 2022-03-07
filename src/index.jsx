4d2e96c154f3be467db0be6c3c158cf74362e27b
Amar <a.zirouel@gmail.com>
2022-03-07 16:39:52 +0100

    First commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestModal from './TestModal'
+
+ReactDOM.render(
+    <div>
+        Hello world <TestModal></TestModal>
+    </div>,
+    document.getElementById('root')
+)
